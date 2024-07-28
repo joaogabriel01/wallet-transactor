27b4cf214ae8b6164dbc212dc658ca1a72f24050
João Azevedo <joaogfazevedo@gmail.com>
2024-07-28 21:38:22 +0000

    feat: complements the domain transactor

--- src/transaction/core/domain/transaction.spec.ts
+++ src/transaction/core/domain/transaction.spec.ts
@@ -1,17 +1,35 @@
 import { Transaction } from './transaction';
 
 describe('Transaction Domain', () => {
-    function runTest(transaction, expectError, expectedResponse) {
+    function runTest(
+        transaction: Transaction,
+        expectError: boolean,
+        expectedResponse: boolean | string,
+        expectedAmountOrigin: number,
+        expectedAmountSender: number,
+        expectedIdOrigin: string,
+        expectedIdSender: string,
+    ) {
+        expect(transaction.getIdOrigin().value).toBe(expectedIdOrigin);
+        expect(transaction.getIdSender().value).toBe(expectedIdSender);
         const response = transaction.execute();
         expect(response.isLeft()).toBe(expectError);
         expect(response.value).toBe(expectedResponse);
+        if (response.isLeft()) {
+            return;
+        }
+        const valueAccountOrigin =
+            transaction.getTheAmountFromAccountOfOrigin();
+        expect(valueAccountOrigin.value).toBe(expectedAmountOrigin);
+        const valueAccountSender = transaction.getTheAmountFromAccountSender();
+        expect(valueAccountSender.value).toBe(expectedAmountSender);
     }
 
     it('should return an error when the amount is greater than the balance in the account', () => {
         const transaction = new Transaction(
-            'accountFrom',
+            'accountOrigin',
             100,
-            'accountTo',
+            'accountSender',
             10,
             1000,
         );
@@ -19,17 +37,29 @@ describe('Transaction Domain', () => {
             transaction,
             true,
             'amount is greater than the balance in the account',
+            0,
+            0,
+            'accountOrigin',
+            'accountSender',
         );
     });
 
     it('should execute successfully when the amount is within the balance in the account', () => {
         const transaction = new Transaction(
-            'accountFrom',
+            'accountOrigin',
             100,
-            'accountTo',
+            'accountSender',
             10,
             10,
         );
-        runTest(transaction, false, true);
+        runTest(
+            transaction,
+            false,
+            true,
+            90,
+            20,
+            'accountOrigin',
+            'accountSender',
+        );
     });
 });
--- src/transaction/core/domain/transaction.ts
+++ src/transaction/core/domain/transaction.ts
@@ -1,31 +1,58 @@
 import { left, right } from '../../../utils/either';
+import { v4 as uuidv4 } from 'uuid';
 
 export class Transaction {
-    private readonly from: string;
-    private readonly accountBalanceFrom: number;
-    private readonly to: string;
+    private readonly id: string;
+    private readonly idOrigin: string;
+    private accountOrigin: number;
+    private readonly idSender: string;
     private readonly value: number;
-    private readonly accountBalanceTo: number;
+    private accountSender: number;
     constructor(
-        from: string,
-        accountBalancerFrom: number,
-        to: string,
-        accountBalanceTo: number,
+        idOrigin: string,
+        accountOrigin: number,
+        idSender: string,
+        accountSender: number,
         value: number,
     ) {
-        this.from = from;
-        this.to = to;
+        this.id = uuidv4();
+        this.idOrigin = idOrigin;
+        this.idSender = idSender;
         this.value = value;
-        this.accountBalanceFrom = accountBalancerFrom;
-        this.accountBalanceTo = accountBalanceTo;
+        this.accountOrigin = accountOrigin;
+        this.accountSender = accountSender;
     }
 
     public execute() {
-        if (this.value > this.accountBalanceFrom) {
+        if (this.value > this.accountOrigin) {
             return left<string, boolean>(
                 'amount is greater than the balance in the account',
             );
         }
+
+        this.accountOrigin -= this.value;
+        this.accountSender += this.value;
+
         return right<string, boolean>(true);
+    }
+
+    public getTheAmountFromAccountOfOrigin() {
+        return right<null, number>(this.accountOrigin);
+    }
+
+    public getTheAmountFromAccountSender() {
+        return right<null, number>(this.accountSender);
+    }
+
+    public getIdTransaction() {
+        return right<null, string>(this.id);
+    }
+
+    public getIdOrigin() {
+        return right<null, string>(this.idOrigin);
+    }
+
+    public getIdSender() {
+        return right<null, string>(this.idSender);
     }
 }
