f565506c04ebbfe8667eb3b7737232094df04d8a
João Azevedo <joaogfazevedo@gmail.com>
2024-08-08 03:19:52 +0000

    fix: adjust side effects

--- src/wallet/wallet.controller.spec.ts
+++ src/wallet/wallet.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WalletController } from './wallet.controller';
 import { WalletService } from './wallet.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Wallet } from './entities/wallet.entity';
 
 describe('WalletController', () => {
     let controller: WalletController;
@@ -8,7 +10,12 @@ describe('WalletController', () => {
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [WalletController],
-            providers: [WalletService],
+            providers: [WalletService, {
+                provide: getRepositoryToken(Wallet),
+                useValue: {
+                    find: jest.fn().mockResolvedValue([])
+                }
+            }],
         }).compile();
 
         controller = module.get<WalletController>(WalletController);
