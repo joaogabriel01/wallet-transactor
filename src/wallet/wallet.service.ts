c2250363851ff474265ff06957e2bec98e9faebe
João Azevedo <joaogfazevedo@gmail.com>
2024-08-10 22:04:44 +0000

    refactor: format prettier in wallet service

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -14,37 +14,41 @@ export class WalletService {
         private walletRepository: Repository<Wallet>,
     ) {}
 
-    async create(createWalletDto: CreateWalletDto): Promise<Either<string, number>> {
+    async create(
+        createWalletDto: CreateWalletDto,
+    ): Promise<Either<string, number>> {
         try {
             const alreadyExists = await this.walletRepository.findOne({
                 where: {
-                    name: createWalletDto.name
-                }
-            })
+                    name: createWalletDto.name,
+                },
+            });
             if (alreadyExists) {
-                return left('User already exists')
+                return left('User already exists');
             }
-    
-            const wallet = this.walletRepository.create(createWalletDto)
-            const newWallet = await this.walletRepository.save(wallet)
-            return right(newWallet.id)
-        } catch (error) {
-            return left('Unexpected error when creating user: ' + error.message);
-        }
 
-        
+            const wallet = this.walletRepository.create(createWalletDto);
+            const newWallet = await this.walletRepository.save(wallet);
+            return right(newWallet.id);
+        } catch (error) {
+            return left(
+                'Unexpected error when creating user: ' + error.message,
+            );
+        }
     }
 
-    async findAll(): Promise<Either<string, WalletOutputDto[]>>{
+    async findAll(): Promise<Either<string, WalletOutputDto[]>> {
         try {
             const allWalletsEntity = await this.walletRepository.find();
-            const wallets: WalletOutputDto[] = []
+            const wallets: WalletOutputDto[] = [];
             for (let i = 0; i < allWalletsEntity.length; i++) {
                 wallets.push(this.toWalletDto(allWalletsEntity[i]));
             }
             return right(wallets);
-        } catch(error) {
-            return left('Unexpected error when find all wallets: '+ error.message)
+        } catch (error) {
+            return left(
+                'Unexpected error when find all wallets: ' + error.message,
+            );
         }
     }
 
@@ -55,9 +59,8 @@ export class WalletService {
                 return left('User not exists');
             }
             return right(this.toWalletDto(wallet));
-        }
-        catch(error) {
-            return left('Unexpected error when findOne: '+ error.message);
+        } catch (error) {
+            return left('Unexpected error when findOne: ' + error.message);
         }
     }
 
