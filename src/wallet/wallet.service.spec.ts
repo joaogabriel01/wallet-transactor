4e8a7e85a0ce3d06d01d3e816bf59beb99561ce1
João Azevedo <joaogfazevedo@gmail.com>
2024-08-10 19:26:38 +0000

    refactor: prettier in wallet spec

--- src/wallet/wallet.service.spec.ts
+++ src/wallet/wallet.service.spec.ts
@@ -6,74 +6,84 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('WalletService', () => {
     let service: WalletService;
-    let walletRepository: Repository<Wallet>
+    let walletRepository: Repository<Wallet>;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [WalletService, {
-                provide: getRepositoryToken(Wallet),
-                useValue: {
-                    find: jest.fn().mockResolvedValue([]),
-                    save: jest.fn().mockResolvedValue([]),
-                    create: jest.fn().mockResolvedValue([]),
-                }
-            }],
+            providers: [
+                WalletService,
+                {
+                    provide: getRepositoryToken(Wallet),
+                    useValue: {
+                        find: jest.fn().mockResolvedValue([]),
+                        save: jest.fn().mockResolvedValue([]),
+                        create: jest.fn().mockResolvedValue([]),
+                    },
+                },
+            ],
         }).compile();
 
         service = module.get<WalletService>(WalletService);
-        walletRepository = module.get<Repository<Wallet>>(getRepositoryToken(Wallet));
+        walletRepository = module.get<Repository<Wallet>>(
+            getRepositoryToken(Wallet),
+        );
     });
 
     it('should be defined', () => {
         expect(service).toBeDefined();
         expect(walletRepository).toBeDefined();
-    })
+    });
 
     it('should return a unexpected error when creating user', async () => {
         const createDtoMock = {
-            ballance: 10, 
-            name: "João",
-            password: "123"
-        }
+            ballance: 10,
+            name: 'João',
+            password: '123',
+        };
         jest.spyOn(walletRepository, 'find').mockImplementationOnce(() => {
             throw new Error('Fake Error');
-          });
-        const response = await service.create(createDtoMock)
+        });
+        const response = await service.create(createDtoMock);
         expect(response.isLeft()).toBe(true);
-        expect(response.value).toBe("Unexpected error when creating user: Fake Error")
+        expect(response.value).toBe(
+            'Unexpected error when creating user: Fake Error',
+        );
     });
 
     it('should return a error if the user exists', async () => {
         const createDtoMock = {
-            ballance: 10, 
-            name: "João",
-            password: "123"
-        }
+            ballance: 10,
+            name: 'João',
+            password: '123',
+        };
         const userMock = {
             id: 1,
-            ballance: 10, 
-            name: "João",
-        }
+            ballance: 10,
+            name: 'João',
+        };
         jest.spyOn(walletRepository, 'find').mockResolvedValueOnce([userMock]);
-        const response = await service.create(createDtoMock)
+        const response = await service.create(createDtoMock);
         expect(response.isLeft()).toBe(true);
-        expect(response.value).toBe("User already exists")
+        expect(response.value).toBe('User already exists');
     });
 
-    it('should return new id if the creation works', async()=>{
+    it('should return new id if the creation works', async () => {
         const createDtoMock = {
-            ballance: 10, 
-            name: "João",
-            password: "123"
-        }
-        jest.spyOn(walletRepository, 'save').mockImplementationOnce(async () => {
-            return {
-                id: 1,
-                ...createDtoMock
-            }
-          });
+            ballance: 10,
+            name: 'João',
+            password: '123',
+        };
+        jest.spyOn(walletRepository, 'save').mockImplementationOnce(
+            async () => {
+                return {
+                    id: 1,
+                    ...createDtoMock,
+                };
+            },
+        );
         const response = await service.create(createDtoMock);
         expect(response.value).toBe(1);
         expect(response.isRight()).toBe(true);
-    })
+    });
+
 });
