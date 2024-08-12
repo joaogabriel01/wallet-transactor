d643f6c203afbb40f81ffcac2682d029a7e0c6ee
João Azevedo <joaogfazevedo@gmail.com>
2024-08-12 00:01:00 +0000

    feat: add auth module

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,67 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import { right } from '../utils/either';
 
 describe('AuthService', () => {
-  let service: AuthService;
+    let service: AuthService;
+    let jwtService: JwtService;
+    let usersService: UsersService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                AuthService,
+                {
+                    provide: JwtService,
+                    useValue: {
+                        signAsync: jest
+                            .fn()
+                            .mockReturnValue('mocked_jwt_token'),
+                    },
+                },
+                {
+                    provide: UsersService,
+                    useValue: {
+                        findOne: jest.fn().mockReturnValue(
+                            right({
+                                id: '1',
+                                username: 'joao',
+                                password: '123',
+                            }),
+                        ),
+                    },
+                },
+            ],
+        }).compile();
+        jwtService = module.get<JwtService>(JwtService);
+        service = module.get<AuthService>(AuthService);
+        usersService = module.get<UsersService>(UsersService);
+    });
 
-    service = module.get<AuthService>(AuthService);
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+        expect(jwtService).toBeDefined();
+        expect(usersService).toBeDefined();
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should return a left when user is incorrect', async () => {
+        jest.spyOn(usersService, 'findOne').mockResolvedValue(
+            right({
+                id: '1',
+                username: 'joao',
+                password: '1234',
+            }),
+        );
+        const token = await service.signIn('joao', '123');
+        expect(token.isLeft()).toBe(true);
+        expect(token.value).toBe('Incorrect username or password');
+    });
+
+    it('should return the token when everything goes well', async () => {
+        const token = await service.signIn('joao', '123');
+        expect(token.isRight()).toBe(true);
+        expect(token.value).toBe('mocked_jwt_token');
+    });
 });
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import { Either, left, right } from '../utils/either';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+    ) {}
+
+    async signIn(
+        username: string,
+        pass: string,
+    ): Promise<Either<string, string>> {
+        const messageUserIncorrect = 'Incorrect username or password';
+        const user = await this.usersService.findOne(username);
+        if (user.isLeft()) {
+            return left(messageUserIncorrect);
+        }
+        const userValues = user.value;
+        if (userValues?.password !== pass) {
+            return left(messageUserIncorrect);
+        }
+        const { password, ...result } = userValues;
+        const payload = { sub: userValues.id, username: userValues.username };
+        const token = await this.jwtService.signAsync(payload);
+        return right(token);
+    }
+}
